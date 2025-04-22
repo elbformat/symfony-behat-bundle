c2a9ff5ae6767e998b8b4e0b0215210aed8977b7
Hauke Sommerfeld <development@haukesomm.de>
2025-04-22 11:39:53 +0200

    Add `when`-step support to `DateContext`

--- src/Context/DateContext.php
+++ src/Context/DateContext.php
@@ -7,11 +7,13 @@ namespace Elbformat\SymfonyBehatBundle\Context;
 use Behat\Behat\Context\Context;
 use Behat\Hook\BeforeScenario;
 use Behat\Step\Given;
+use Behat\Step\When;
 use SlopeIt\ClockMock\ClockMock;
 
 /**
- * Use ClockMock (https://github.com/slope-it/clock-mock) with uopz to mock the current date.
- * ! Make sure the ext-uopz is installed and activated !
+ * Use [ClockMock](https://github.com/slope-it/clock-mock) with uopz to mock the current date.
+ *
+ * __Make sure the ext-uopz is installed and activated!__
  *
  * @author Hannes Giesenow <hannes.giesenow@elbformat.de>
  */
@@ -24,6 +26,7 @@ class DateContext implements Context
     }
 
     #[Given('the current date is :date')]
+    #[When('I set the date to :timestamp')]
     public function theCurrentDateIs(string $date): void
     {
         ClockMock::freeze(new \DateTime($date));
